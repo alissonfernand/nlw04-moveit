73cdef14226da7626919d815f8a4703fe32f52bc
alissonfernandes <alisson.flutter@gmail.com>
2021-02-26 17:39:58 -0300

    usando o context pra exibir o level

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../context/ChallengesContext';
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile () {
+  const {level} = useContext(ChallengesContext)
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/alissonfernand.png" alt="Alisson Fernandes"/>
@@ -9,7 +13,7 @@ export function Profile () {
         <strong>Alisson Fernandes</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
