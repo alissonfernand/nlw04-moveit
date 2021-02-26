b74dab4b778d934e0c8eea3c7b6553a48c0502ac
alissonfernandes <alisson.flutter@gmail.com>
2021-02-26 17:00:33 -0300

    add challengesprovides global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/global.css'
 
+import {ChallengesProdiver} from '../context/ChallengesContext';
+
 // component global
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // add ChallengesProdiver global
+    <ChallengesProdiver>
+      <Component {...pageProps} />
+    </ChallengesProdiver>
+  )
 }
 
 export default MyApp
