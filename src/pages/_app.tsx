acb27710dcabd4ab43c39529674822a79e2ccc33
alissonfernandes <alisson.flutter@gmail.com>
2021-02-27 20:46:21 -0300

    removendo o challengesprovides global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,9 @@
 import '../styles/global.css'
 
-import {ChallengesProdiver} from '../context/ChallengesContext';
 
-// component global
 function MyApp({ Component, pageProps }) {
   return (
-    // add ChallengesProdiver global
-    <ChallengesProdiver>
       <Component {...pageProps} />
-    </ChallengesProdiver>
   )
 }
 
