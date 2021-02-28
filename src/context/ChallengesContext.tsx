796f39c4f829d090a5f2bdfaae494a00dfb9eb80
alissonfernandes <alisson.flutter@gmail.com>
2021-02-27 22:04:56 -0300

    metodos para exibir e fechar o modal

--- src/context/ChallengesContext.tsx
+++ src/context/ChallengesContext.tsx
@@ -3,6 +3,7 @@ import {createContext, useState, ReactNode, useEffect } from 'react';
 import Cookie from 'js-cookie';
 
 import challenges from '../../challenges.json';
+import { LevelUpModal } from '../components/LevelUpModal';
 
 
 interface Challenge {
@@ -21,6 +22,7 @@ interface ChallengesContextData {
   activeChallenge: Challenge;
   resetChallenge: () => void;
   completeChallenge: () => void;
+  closeLevelUpModal: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -36,8 +38,10 @@ export function ChallengesProdiver ({children, ...rest}: ChallengesProviderProps
   const [level, setLevel] = useState(rest.level ?? 1)
   const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0)
   const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0)
+  
 
   const [activeChallenge, setActiveChallenge] = useState(null)
+  const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false)
 
   const experienceToNextLevel = Math.pow((level + 1 ) * 4, 2)
 
@@ -55,6 +59,11 @@ export function ChallengesProdiver ({children, ...rest}: ChallengesProviderProps
 
   function levelUp() {
     setLevel(level + 1)
+    setIsLevelUpModalOpen(true)
+  }
+
+  function closeLevelUpModal() {
+    setIsLevelUpModalOpen(false)
   }
 
   function startNewChallenge() {
@@ -107,9 +116,12 @@ export function ChallengesProdiver ({children, ...rest}: ChallengesProviderProps
         startNewChallenge,
         activeChallenge,
         resetChallenge,
-        completeChallenge 
+        completeChallenge,
+        closeLevelUpModal 
       }}>
       {children}
+      
+      {isLevelUpModalOpen && <LevelUpModal />}
     </ChallengesContext.Provider>
   )
 }
